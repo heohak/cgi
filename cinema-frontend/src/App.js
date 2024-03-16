fd3095d60fefce6d9627a30ee4c9fb2b8185dc9c
heohak <heohak@ttu.ee>
2024-03-17 00:16:01 +0200

    reacti app cors päringute lubamine

---
+++ cinema-frontend/src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import Films from "./components/Films";
+import AddFilm from "./components/AddFilm";
+
+function App() {
+  return (
+      <div>
+        <h1>Kino Rakendus</h1>
+        <Films />
+        <AddFilm />
+      </div>
+  );
+}
+
+export default App;
--- src/main/java/org/example/cgi/CgiApplication.java
+++ src/main/java/org/example/cgi/CgiApplication.java
@@ -2,12 +2,26 @@ package org.example.cgi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+@Configuration
 @SpringBootApplication
 public class CgiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CgiApplication.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/**").allowedOrigins("http://localhost:3000");
+            }
+        };
     }
 
 }
